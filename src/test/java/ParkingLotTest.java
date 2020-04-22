834750f4d33296f77d5c3cab0964c6e1dbc3e251
Revati Tekale <revitekale1910@gmail.com>
2020-04-22 18:48:17 +0530

    ADDED:Test case to unpark the vehicle

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -30,4 +30,24 @@ public class ParkingLotTest {
           Assert.assertEquals("Parking is full", e.getMessage());
         }
     }
+
+    @Test
+    public void givenVehicle_WhenUnPark_ThenTrue() throws ParkingLotException {
+        parkingLotMain.parkVehicle(vehicle);
+        boolean result=parkingLotMain.unParkVehicle(vehicle);
+        Assert.assertEquals(true,result);
+    }
+
+    @Test
+    public void givenVehicle_WhenAlreadyUnPark_ThenFalse(){
+        try {
+            parkingLotMain.parkVehicle(vehicle);
+            boolean result=parkingLotMain.unParkVehicle(new Object());
+            Assert.assertEquals(false,result);
+        } catch (ParkingLotException e) {
+           Assert.assertEquals("No Vehicle found", e.getMessage());
+        }
+    }
+
+
 }
