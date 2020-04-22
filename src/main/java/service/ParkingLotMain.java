b18f6f5f85a01812986a145f52344067d0812a6d
Revati Tekale <revitekale1910@gmail.com>
2020-04-22 23:31:24 +0530

    ADDED:Method to inform owner

--- src/main/java/service/ParkingLotMain.java
+++ src/main/java/service/ParkingLotMain.java
@@ -1,16 +1,41 @@
 package service;
 
 import exception.ParkingLotException;
+import observer.Observer;
+import observer.Owner;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 public class ParkingLotMain {
 
     Object vehicle;
+    LinkedHashMap<String, Object> parkingLot = new LinkedHashMap<String, Object>();
+    private List<Observer> observableList = new ArrayList<>();
+    Owner owner = new Owner();
+    private String isFull;
+
+    public void addObserver(Observer observable) {
+        this.observableList.add(observable);
+    }
+
+    public void setStatus(String isFull) {
+        this.isFull = isFull;
+        for (Observer observable : this.observableList) {
+            observable.update(this.isFull);
+        }
+    }
 
     //PARK VEHICLE
-    public void parkVehicle(Object vehicle) throws ParkingLotException {
-        if(this.vehicle != null)
-            throw new ParkingLotException("Parking is full", ParkingLotException.ExceptionType.PARKING_FULL);
+    public String parkVehicle(VehiclesPojo vehicle) throws ParkingLotException {
+        if (parkingLot.size()%2==0 && parkingLot.size() != 0) {
+            parkingLot = null;
+            throw new ParkingLotException("Parking lot is full", ParkingLotException.ExceptionType.PARKING_FULL);
+        }
         this.vehicle = vehicle;
+        parkingLot.put(vehicle.getVehicleNumber(), vehicle);
+        return null;
     }
 
     //CHECK VEHICLE IS PARKED OR NOT
@@ -20,9 +45,11 @@ public class ParkingLotMain {
         return false;
     }
 
-    public boolean unParkVehicle(Object vehicle) throws ParkingLotException {
+    public boolean unParkVehicle(VehiclesPojo vehicle) throws ParkingLotException {
+        if (vehicle == null) return false;
         if(this.vehicle.equals(vehicle)) {
             this.vehicle = null;
+            parkingLot.remove(vehicle.getVehicleNumber());
             return true;
         }
         throw new ParkingLotException("No Vehicle found", ParkingLotException.ExceptionType.NO_VEHICLE);
