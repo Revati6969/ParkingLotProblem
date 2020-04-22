3dc7ff946c788a2842378b179514e9abd4cb8fe3
Revati Tekale <revitekale1910@gmail.com>
2020-04-22 23:34:56 +0530

    ADDED:Test case to inform Owner

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -1,21 +1,28 @@
 import exception.ParkingLotException;
+import observer.Owner;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import service.ParkingLotMain;
+import service.VehiclesPojo;
 
 public class ParkingLotTest {
     ParkingLotMain parkingLotMain = null;
-    Object vehicle = null;
+    VehiclesPojo vehicle = null;
+    Owner owner = null;
+
 
     @Before
     public void setUp() {
-        vehicle = new Object();
+        vehicle = null;
         parkingLotMain = new ParkingLotMain();
+        owner = new Owner();
     }
 
     @Test
     public void givenVehicle_WhenParked_ReturnTrue() throws ParkingLotException {
+        vehicle.setVehicleName("abc");
+        vehicle.setVehicleNumber("11");
         parkingLotMain.parkVehicle(vehicle);
         boolean result = parkingLotMain.isVehiclePark(vehicle);
         Assert.assertTrue(result);
@@ -25,27 +32,50 @@ public class ParkingLotTest {
     public void givenVehicle_WhenAlreadyParked_ShouldThrowException() {
         try {
             parkingLotMain.parkVehicle(vehicle);
-            parkingLotMain.parkVehicle(new Object());
+            parkingLotMain.parkVehicle(vehicle);
         } catch (ParkingLotException e) {
-          Assert.assertEquals("Parking is full", e.getMessage());
+            Assert.assertEquals("Parking is full", e.getMessage());
         }
     }
 
     @Test
     public void givenVehicle_WhenUnPark_ThenTrue() throws ParkingLotException {
         parkingLotMain.parkVehicle(vehicle);
-        boolean result=parkingLotMain.unParkVehicle(vehicle);
-        Assert.assertEquals(true,result);
+        boolean result = parkingLotMain.unParkVehicle(vehicle);
+        Assert.assertEquals(true, result);
     }
 
     @Test
-    public void givenVehicle_WhenAlreadyUnPark_ThenFalse(){
+    public void givenParkingLot_WhenFull_ThenReturnTrue() {
         try {
+            VehiclesPojo vehicle = new VehiclesPojo();
+            vehicle.setVehicleName("abc");
+            vehicle.setVehicleNumber("11");
             parkingLotMain.parkVehicle(vehicle);
-            boolean result=parkingLotMain.unParkVehicle(new Object());
-            Assert.assertEquals(false,result);
+            vehicle.setVehicleName("abc");
+            vehicle.setVehicleNumber("12");
+            parkingLotMain.parkVehicle(vehicle);
+            parkingLotMain.parkVehicle(vehicle);
         } catch (ParkingLotException e) {
-           Assert.assertEquals("No Vehicle found", e.getMessage());
+            Assert.assertEquals("Parking lot is full", e.getMessage());
+        }
+    }
+
+    @Test
+    public void givenParkingLotIsFull_WhenInformOwner_ThenReturnTrue() {
+        try {
+            parkingLotMain.addObserver(owner);
+            VehiclesPojo vehicle1 = new VehiclesPojo();
+            vehicle1.setVehicleName("abc");vehicle1.setVehicleNumber("45");
+            parkingLotMain.parkVehicle(vehicle1);
+            VehiclesPojo vehicle2 = new VehiclesPojo();
+            vehicle2.setVehicleName("abc");vehicle2.setVehicleNumber("47");
+            parkingLotMain.parkVehicle(vehicle2);
+            vehicle.setVehicleName("abc");vehicle.setVehicleNumber("48");
+            parkingLotMain.parkVehicle(vehicle);
+            Assert.assertEquals("Full Lot 1",owner.getParkingSlotFullOrNot());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
